0b4cd90f429fc052f0c1f3b71e60b36c4c6bf4fd
Musenzi <admin@musenzi.be>
2020-04-25 10:34:39 +0200

    Changed Zilliqa Object

--- MusZil_Core/Zilliqa.cs
+++ MusZil_Core/Zilliqa.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using MusZil_Core.API;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MusZil_Core
 {
     public class Zilliqa
     {
+        private MusZil_APIClient _client;
+        public Zilliqa()
+        {
+            
+        }
+        public Zilliqa(MusZil_APIClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<string> GetBalanceForAddress(string address)
+        {
+            var req = new MusRequest("GetBalance",address);
+            return await _client.CallMethod(req);
+        }
     }
 }
