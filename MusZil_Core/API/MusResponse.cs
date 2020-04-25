827da37ab28059f83fb423c98871edfab408fdaa
Musenzi <admin@musenzi.be>
2020-04-25 10:22:06 +0200

    Added APIClient +  Response & Result objects

---
+++ MusZil_Core/API/MusResponse.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusZil_Core.API
+{
+    public class MusResponse
+    {
+      
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [JsonProperty("jsonrpc")]
+            public string Jsonrpc { get; set; }
+
+            [JsonProperty("result")]
+            public object Result { get; set; }
+        
+    }
+}
