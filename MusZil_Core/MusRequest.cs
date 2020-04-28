a82a70c0b514e85a1606d5a026907852399bae3f
Musenzi <admin@musenzi.be>
2020-04-28 08:11:06 +0200

    https://github.com/musenzi/Zilliqa-API-CSharp/pull/3

---
+++ MusZil_Core/MusRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusZil_Core
+{
+    public class MusRequest
+    {
+        public MusRequest(string method, string param)
+        {
+            Id = 1;
+            Method = method;
+            Parameters = param;
+        }
+
+        public int Id { get; set; }
+        public string Jsonrpc { get; set; }
+        public string Method { get; set; }
+        public object Parameters { get; set; }
+
+        public string ToJson()
+        {
+            return $"{{\r\n    \"id\": \"{Id}\",\r\n    \"jsonrpc\": \"2.0\",\r\n    \"method\": \"{Method}\",\r\n    \"params\": [\"{Parameters}\"]\r\n}}";
+        }
+    }
+}
